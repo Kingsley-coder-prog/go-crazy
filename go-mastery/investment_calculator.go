76ea437c04a2de7622234ce5f82b202980da66fd
Kingsley <kifeanyi216@gmail.com>
2026-06-19 11:48:57 +0100

    Using ALternative Variable Declaration Style

--- go-mastery/investment_calculator.go
+++ go-mastery/investment_calculator.go
@@ -6,11 +6,11 @@ import (
 )
 
 func main() {
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	var investmentAmount float64 = 1000
+	years := 10.0
+	expectedReturnRate := 5.5
 
-	var futureValue = float64(investmentAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
 
 	fmt.Println("Future value of the investment: ", futureValue)
 }
