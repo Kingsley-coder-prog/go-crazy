518d36402d5cd0862452dcf1991445bea5d58aa3
Kingsley <kifeanyi216@gmail.com>
2026-06-22 08:43:49 +0100

    Variable Importance

--- go-mastery/investment_calculator.go
+++ go-mastery/investment_calculator.go
@@ -6,11 +6,16 @@ import (
 )
 
 func main() {
-	var investmentAmount float64 = 1000
+	const inflationRate = 2.5
+	var investmentAmount float64
 	years := 10.0
 	expectedReturnRate := 5.5
 
+	fmt.Scan(&investmentAmount)
+
 	futureValue := (investmentAmount) * math.Pow(1+expectedReturnRate/100, (years))
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
 
 	fmt.Println("Future value of the investment: ", futureValue)
+	fmt.Println("Future real value of the investment: ", futureRealValue)
 }
