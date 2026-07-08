8bc63451a961ed7a723fd727d73f50bc539d6a60
Kingsley <kifeanyi216@gmail.com>
2026-07-08 12:11:25 +0100

    Making sense of switch statements

--- go-basics-dev/bank.go
+++ go-basics-dev/bank.go
@@ -19,10 +19,10 @@ func main() {
 
 		// wantsCheckBalance := choice == 1
 
-		if choice == 1 {
+		switch choice {
+		case 1:
 			fmt.Println("Your account balance is: $", accountBalance)
-
-		} else if choice == 2 {
+		case 2:
 			fmt.Print("Your deposit: $")
 			var depositAmount float64
 			fmt.Scan(&depositAmount)
@@ -35,28 +35,30 @@ func main() {
 
 			accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
 			fmt.Println("Balance updated! New amount: $", accountBalance)
-		} else if choice == 3 {
+		case 3:
 			fmt.Print("Your withdraw: $")
 			var withdrawAmount float64
 			fmt.Scan(&withdrawAmount)
 
 			if withdrawAmount <= 0 {
 				fmt.Println("Invalid amount. Must be greater than 0.")
-				return
+				continue
 			}
 
 			if withdrawAmount > accountBalance {
 				fmt.Println("Insufficient funds. Your account balance is: $", accountBalance)
-				return
+				continue
 			}
 
 			accountBalance -= withdrawAmount // accountBalance = accountBalance - withdrawAmount
 			fmt.Println("Balance updated! New amount: $", accountBalance)
-		} else {
+		default:
 			fmt.Println("Goodbye!")
-			break
+			fmt.Println("Thank you for using Go Bank!")
+			// break
+			return
 		}
+
 	}
-	fmt.Println("Thank you for using Go Bank!")
 
 }
