18db2c146dfc0523b82ba9fd683344fcb8429fc9
Kingsley <kifeanyi216@gmail.com>
2026-07-10 12:55:57 +0100

    Using all operations in a simple bank app

--- go-basics-practice-1/profit_calculator.go
+++ go-basics-practice-1/profit_calculator.go
@@ -1,18 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+// This program calculates the profit of a business based on user input for revenue, expenses, and tax rate. It also stores the results in a file named "results.txt".
+// Goals
+// 1) Validate user input
+//  => Show error message & exit if invalid input is provided
+//  - No negative numbers
+//  - Not 0
+// 2) Store calculated results into file
 
 func main() {
 
-	revenue := getUserInput("Revenue: ")
-	expenses := getUserInput("Expenses: ")
-	taxRate := getUserInput("Tax Rate:")
+	revenue, err := getUserInput("Revenue: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+		// panic(err)
+	}
+
+	expenses, err := getUserInput("Expenses: ")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	taxRate, err := getUserInput("Tax Rate:")
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	ebt, profit, ratio := calculateFinancials(revenue, expenses, taxRate)
 
 	fmt.Printf("%.1f\n", ebt)
 	fmt.Printf("%.1f\n", profit)
 	fmt.Printf("%.3f\n", ratio)
+	storeResults(ebt, profit, ratio)
+}
+
+func storeResults(ebt, profit, ratio float64) {
+	results := fmt.Sprintf("EBT: %.1f\nProfit: %.1f\nRatio: %.3f\n", ebt, profit, ratio)
+	os.WriteFile("results.txt", []byte(results), 0644)
 }
 
 func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, float64) {
@@ -23,10 +59,14 @@ func calculateFinancials(revenue, expenses, taxRate float64) (float64, float64, 
 	return ebt, profit, ratio
 }
 
-func getUserInput(infoText string) float64 {
+func getUserInput(infoText string) (float64, error) {
 	var userInput float64
 	fmt.Print(infoText)
 	fmt.Scan(&userInput)
 
-	return userInput
+	if userInput <= 0 {
+		return 0, errors.New("Value must be a positive number.")
+	}
+
+	return userInput, nil
 }
