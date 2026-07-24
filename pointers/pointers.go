107312b1d5ebd969e677aecec5d78cb38fa4fec5
Kingsley <kifeanyi216@gmail.com>
2026-07-24 08:52:14 +0100

    creating a pointer & pointers as values

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -5,10 +5,13 @@ import "fmt"
 func main() {
 	age := 32 // regular variable
 
-	fmt.Println("Age: ", age)
+	var agePointer *int // pointer variable declaration
+	agePointer = &age   // pointer variable
 
-	adultYears := getAdultYears(age)
-	fmt.Println("Adult years: ", adultYears)
+	fmt.Println("Age: ", *agePointer) // prints the value of age variable
+
+	// adultYears := getAdultYears(age)
+	// fmt.Println("Adult years: ", adultYears)
 }
 
 func getAdultYears(age int) int {
